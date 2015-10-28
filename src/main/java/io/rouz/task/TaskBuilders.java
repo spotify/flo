200f43cdc306e56f76b2dcc5164a9815cb13bcfc
Rouzbeh Delavari <rouz@spotify.com>
2015-10-28 21:06:22 +0100

    clean up taskbuilders

--- src/main/java/io/rouz/task/TaskBuilders.java
+++ src/main/java/io/rouz/task/TaskBuilders.java
@@ -34,15 +34,170 @@ final class TaskBuilders {
   }
 
   static TaskBuilder rootBuilder(String taskName, Object[] args) {
-    return new TB(taskName, args);
+    return new Builder0(taskName, args);
   }
 
-  private interface Lifter<F> extends F1<F, F1<TaskContext, ?>> {
-    default <R> F1<TaskContext, R> liftWithCast(F fn) {
-      // force the return type of the lifter function to R
-      // not type safe, but isolated to this file
-      return (F1<TaskContext, R>) this.apply(fn);
+  // #############################################################################################
+
+  private static class Builder0
+      extends BaseRefs<Void>
+      implements TaskBuilder {
+
+    Builder0(String taskName, Object[] args) {
+      super(taskName, args);
     }
+
+    @Override
+    public <R> Task<R> process(F0<R> code) {
+      return Task.create(toStream(), tc -> code.get(), taskName, args);
+    }
+
+    @Override
+    public <A> TaskBuilder1<A> in(F0<Task<A>> aTask) {
+      return new Builder1<>(
+          toStream(aTask),
+          taskName, args,
+          f1 -> tc -> f1.apply(
+              aTask.get().internalOut(tc)));
+    }
+
+    @Override
+    public <A> TaskBuilder1<List<A>> ins(F0<Stream<Task<A>>> aTasks) {
+      return new Builder1<>(
+          toFlatStream(aTasks),
+          taskName, args,
+          f1 -> tc -> f1.apply(
+              aTasks.get().map(t -> t.internalOut(tc)).collect(toList())));
+    }
+  }
+
+  // #############################################################################################
+
+  private static class Builder1<A>
+      extends BaseRefs<F1<A, ?>>
+      implements TaskBuilder1<A> {
+
+    Builder1(Stream<Task<?>> tasks, String taskName, Object[] args, Lifter<F1<A, ?>> lifter) {
+      super(tasks, lifter, taskName, args);
+    }
+
+
+    @Override
+    public <R> Task<R> process(F1<A, R> code) {
+      return Task.create(tasks, lifter.liftWithCast(code), taskName, args);
+    }
+
+    @Override
+    public <B> TaskBuilder2<A, B> in(F0<Task<B>> bTask) {
+      return new Builder2<>(
+          concat(tasks, toStream(bTask)),
+          taskName, args,
+          lifter.mapWithContext(
+              (tc, f2) -> a -> f2.apply(
+                  a,
+                  bTask.get().internalOut(tc))));
+    }
+
+    @Override
+    public <B> TaskBuilder2<A, List<B>> ins(F0<Stream<Task<B>>> bTasks) {
+      return new Builder2<>(
+          concat(tasks, toFlatStream(bTasks)),
+          taskName, args,
+          lifter.mapWithContext(
+              (tc, f2) -> a -> f2.apply(
+                  a,
+                  bTasks.get().map(t -> t.internalOut(tc)).collect(toList()))));
+    }
+  }
+
+  // #############################################################################################
+
+  private static class Builder2<A, B>
+      extends BaseRefs<F2<A, B, ?>>
+      implements TaskBuilder2<A, B> {
+
+    Builder2(Stream<Task<?>> tasks, String taskName, Object[] args, Lifter<F2<A, B, ?>> lifter) {
+      super(tasks, lifter, taskName, args);
+    }
+
+    @Override
+    public <R> Task<R> process(F2<A, B, R> code) {
+      return Task.create(tasks, lifter.liftWithCast(code), taskName, args);
+    }
+
+    @Override
+    public <C> TaskBuilder3<A, B, C> in(F0<Task<C>> cTask) {
+      return new Builder3<>(
+          concat(tasks, toStream(cTask)),
+          taskName, args,
+          lifter.mapWithContext(
+              (tc, f3) -> (a, b) -> f3.apply(
+                  a, b,
+                  cTask.get().internalOut(tc))));
+    }
+
+    @Override
+    public <C> TaskBuilder3<A, B, List<C>> ins(F0<Stream<Task<C>>> cTasks) {
+      return new Builder3<>(
+          concat(tasks, toFlatStream(cTasks)),
+          taskName, args,
+          lifter.mapWithContext(
+              (tc, f3) -> (a, b) -> f3.apply(
+                  a, b,
+                  cTasks.get().map(t -> t.internalOut(tc)).collect(toList()))));
+    }
+  }
+
+  // #############################################################################################
+
+  private static class Builder3<A, B, C>
+      extends BaseRefs<F3<A, B, C, ?>>
+      implements TaskBuilder3<A, B, C> {
+
+    Builder3(Stream<Task<?>> tasks, String taskName, Object[] args, Lifter<F3<A, B, C, ?>> lifter) {
+      super(tasks, lifter, taskName, args);
+    }
+
+    @Override
+    public <R> Task<R> process(F3<A, B, C, R> code) {
+      return Task.create(tasks, lifter.liftWithCast(code), taskName, args);
+    }
+  }
+
+  // #############################################################################################
+
+  private static class BaseRefs<F> {
+
+    protected final Stream<Task<?>> tasks;
+    protected final Lifter<F> lifter;
+    protected final String taskName;
+    protected final Object[] args;
+
+    protected BaseRefs(Stream<Task<?>> tasks, Lifter<F> lifter, String taskName, Object[] args) {
+      this.tasks = tasks;
+      this.lifter = lifter;
+      this.taskName = taskName;
+      this.args = args;
+    }
+
+    protected BaseRefs(String taskName, Object[] args) {
+      this.tasks = null;
+      this.lifter = null;
+      this.taskName = taskName;
+      this.args = args;
+    }
+  }
+
+  // #############################################################################################
+
+  /**
+   * Higher order function that lifts an arbitrary function {@link F} into a function from
+   * {@link TaskContext} to an unknown type.
+   *
+   * @param <F>  The type of the function that can be lifted
+   */
+  @FunctionalInterface
+  private interface Lifter<F> extends F1<F, F1<TaskContext, ?>> {
 
     /**
      * Maps this {@link Lifter} into a {@link Lifter} of a different function based on a function
@@ -58,189 +213,24 @@ final class TaskBuilders {
      * @param <G>    The function type of the new lifter
      * @return A new lifter that can lift functions of type {@link G}
      */
-    default <G> Lifter<G> mapWith(F2<TaskContext, G, F> mapFn) {
+    default <G> Lifter<G> mapWithContext(F2<TaskContext, G, F> mapFn) {
       return g -> tc -> this.apply(mapFn.apply(tc, g)).apply(tc);
     }
-  }
 
-  private static class BaseTask {
-    protected final String taskName;
-
-    protected final Object[] args;
-    protected BaseTask(String taskName, Object[] args) {
-      this.taskName = taskName;
-      this.args = args;
-    }
-
-  }
-
-  // #############################################################################################
-
-  private static class TB
-      extends BaseTask
-      implements TaskBuilder {
-
-    TB(String taskName, Object[] args) {
-      super(taskName, args);
-    }
-
-    @Override
-    public <R> Task<R> process(F0<R> code) {
-      return Task.create(toStream(), tc -> code.get(), taskName, args);
-    }
-
-    @Override
-    public <A> TaskBuilder1<A> in(F0<Task<A>> aTask) {
-      return new TB1<>(
-          toStream(aTask),
-          taskName, args,
-          f1 -> tc -> f1.apply(
-              aTask.get().internalOut(tc)));
-    }
-
-    @Override
-    public <A> TaskBuilder1<List<A>> ins(F0<Stream<Task<A>>> aTasks) {
-      return new TB1<>(
-          toFlatStream(aTasks),
-          taskName, args,
-          f1 -> tc -> f1.apply(
-              aTasks.get().map(t -> t.internalOut(tc)).collect(toList())));
+    /**
+     * Lift a function {@code fn} while casting the return type. This is the only place we do
+     * this cast.
+     *
+     * @param fn   Function to lift
+     * @param <R>  The forced return type
+     * @return A function from {@link TaskContext} to the forced type
+     */
+    default <R> F1<TaskContext, R> liftWithCast(F fn) {
+      // force the return type of the lifter function to R
+      // not type safe, but isolated to this file
+      return (F1<TaskContext, R>) this.apply(fn);
     }
   }
-
-  // #############################################################################################
-
-  private static class TB1<A>
-      extends BaseTask
-      implements TaskBuilder1<A> {
-
-    private final Stream<? extends Task<?>> tasks;
-    private final Lifter<F1<A, ?>> lifter;
-
-    TB1(
-        Stream<? extends Task<?>> tasks,
-        String taskName, Object[] args,
-        Lifter<F1<A, ?>> lifter) {
-      super(taskName, args);
-      this.tasks = tasks;
-      this.lifter = lifter;
-    }
-
-
-    @Override
-    public <R> Task<R> process(F1<A, R> code) {
-      F1<TaskContext, R> f = lifter.liftWithCast(code);
-
-      return Task.create(
-          tasks,
-          f::apply,
-          taskName, args);
-    }
-
-    @Override
-    public <B> TaskBuilder2<A, B> in(F0<Task<B>> bTask) {
-      return new TB2<>(
-          concat(tasks, toStream(bTask)),
-          taskName, args,
-          lifter.mapWith(
-              (tc, f2) -> a -> f2.apply(
-                  a,
-                  bTask.get().internalOut(tc))));
-    }
-
-    @Override
-    public <B> TaskBuilder2<A, List<B>> ins(F0<Stream<Task<B>>> bTasks) {
-      return new TB2<>(
-          concat(tasks, toFlatStream(bTasks)),
-          taskName, args,
-          lifter.mapWith(
-              (tc, f2) -> a -> f2.apply(
-                  a,
-                  bTasks.get().map(t -> t.internalOut(tc)).collect(toList()))));
-    }
-  }
-
-  // #############################################################################################
-
-  private static class TB2<A, B>
-      extends BaseTask
-      implements TaskBuilder2<A, B> {
-
-    private final Stream<? extends Task<?>> tasks;
-    private final Lifter<F2<A, B, ?>> lifter;
-
-    TB2(
-        Stream<? extends Task<?>> tasks,
-        String taskName, Object[] args,
-        Lifter<F2<A, B, ?>> lifter) {
-      super(taskName, args);
-      this.tasks = tasks;
-      this.lifter = lifter;
-    }
-
-    @Override
-    public <R> Task<R> process(F2<A, B, R> code) {
-      F1<TaskContext, R> f = lifter.liftWithCast(code);
-
-      return Task.create(
-          tasks,
-          f::apply,
-          taskName, args);
-    }
-
-    @Override
-    public <C> TaskBuilder3<A, B, C> in(F0<Task<C>> cTask) {
-      return new TB3<>(
-          concat(tasks, toStream(cTask)),
-          taskName, args,
-          lifter.mapWith(
-              (tc, f3) -> (a, b) -> f3.apply(
-                  a, b,
-                  cTask.get().internalOut(tc))));
-    }
-
-    @Override
-    public <C> TaskBuilder3<A, B, List<C>> ins(F0<Stream<Task<C>>> cTasks) {
-      return new TB3<>(
-          concat(tasks, toFlatStream(cTasks)),
-          taskName, args,
-          lifter.mapWith(
-              (tc, f3) -> (a, b) -> f3.apply(
-                  a, b,
-                  cTasks.get().map(t -> t.internalOut(tc)).collect(toList()))));
-    }
-  }
-
-  // #############################################################################################
-
-  private static class TB3<A, B, C>
-      extends BaseTask
-      implements TaskBuilder3<A, B, C> {
-
-    private final Stream<? extends Task<?>> tasks;
-    private final Lifter<F3<A, B, C, ?>> lifter;
-
-    TB3(
-        Stream<? extends Task<?>> tasks,
-        String taskName, Object[] args,
-        Lifter<F3<A, B, C, ?>> lifter) {
-      super(taskName, args);
-      this.tasks = tasks;
-      this.lifter = lifter;
-    }
-
-    @Override
-    public <R> Task<R> process(F3<A, B, C, R> code) {
-      F1<TaskContext, R> f = lifter.liftWithCast(code);
-
-      return Task.create(
-          tasks,
-          f::apply,
-          taskName, args);
-    }
-  }
-
-  // #############################################################################################
 
   /**
    * Converts an array of {@link F0}s of {@link Task}s to a {@link Stream} of the same
