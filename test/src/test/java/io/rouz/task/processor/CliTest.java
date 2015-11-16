b1fc366f77a5d9ff00cc4ac78a7c8cdaa35fb428
Rouzbeh Delavari <rouz@spotify.com>
2015-11-16 22:22:57 +0100

    test parsing for different argument types

--- test/src/test/java/io/rouz/task/processor/CliTest.java
+++ test/src/test/java/io/rouz/task/processor/CliTest.java
@@ -16,28 +16,92 @@ public class CliTest {
   private static String secondString;
   private static boolean firstFlag = false;
   private static boolean secondFlag = true;
-
-  private String[] args = {
-      "create", "--first", "22", "--flag1", "--second", "hello"};
+  private static CustomEnum parsedEnum;
+  private static CustomType parsedType;
 
   @Test
-  public void testName() throws Exception {
-    Cli.forFactories(FloRootTaskFactory.CliTest_TestTask())
-        .run(args);
+  public void shouldParseStandardArgTypes() throws Exception {
+    Cli.forFactories(FloRootTaskFactory.CliTest_StandardArgs())
+        .run("create", "--first", "22", "--second", "hello");
 
     assertThat(firstInt, is(22));
     assertThat(secondString, is("hello"));
+  }
+
+  @RootTask
+  public static Task<String> standardArgs(int first, String second) {
+    firstInt = first;
+    secondString = second;
+    return Task.named("StandardArgs", first, second)
+        .process(() -> second + " " + first * 100);
+  }
+
+  @Test
+  public void shouldParseFlags() throws Exception {
+    Cli.forFactories(FloRootTaskFactory.CliTest_Flags())
+        .run("create", "--flag1");
+
     assertTrue(firstFlag); // toggled
     assertFalse(secondFlag);  // toggled
   }
 
   @RootTask
-  public static Task<String> testTask(int first, boolean flag1, String second, boolean flag2) {
-    firstInt = first;
-    secondString = second;
+  public static Task<String> flags(boolean flag1, boolean flag2) {
     firstFlag = flag1;
     secondFlag = flag2;
-    return Task.named("Test1", first, second)
-        .process(() -> second + " " + first * 100);
+    return Task.named("Flags", flag1, flag2)
+        .process(() -> flag1 + " " + flag2);
+  }
+
+  @Test
+  public void shouldParseEnums() throws Exception {
+    Cli.forFactories(FloRootTaskFactory.CliTest_Enums())
+        .run("create", "--enm", "BAR");
+
+    assertThat(parsedEnum, is(CustomEnum.BAR));
+  }
+
+  @RootTask
+  public static Task<String> enums(CustomEnum enm) {
+    parsedEnum = enm;
+    return Task.named("Enums", enm)
+        .process(enm::toString);
+  }
+
+  @Test
+  public void shouldParseCustomTypes() throws Exception {
+    Cli.forFactories(FloRootTaskFactory.CliTest_CustomType())
+        .run("create", "--myType", "blarg");
+
+    assertThat(parsedType.content, is("blarg parsed for you!"));
+  }
+
+  @RootTask
+  public static Task<String> customType(CustomType myType) {
+    parsedType = myType;
+    return Task.named("Types", myType.content)
+        .process(() -> myType.content);
+  }
+
+  public enum CustomEnum {
+    BAR
+  }
+
+  public static class CustomType {
+    final String content;
+
+    CustomType(String content) {
+      this.content = content;
+    }
+
+    /**
+     * String parser that JOpt Simple will use to parse values of {@link CustomType}
+     *
+     * @param string  The command line string to be parsed
+     * @return An instance of {@link CustomType}
+     */
+    public static CustomType valueOf(String string) {
+      return new CustomType(string + " parsed for you!");
+    }
   }
 }
