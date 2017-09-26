45e443e0993c07675657ffe06c4403fdb5b2a673
Rouzbeh Delavari <rouzwawi@gmail.com>
2017-09-26 14:41:22 +0200

    Update site update script

--- update-site.sh
+++ update-site.sh
@@ -11,7 +11,7 @@ git checkout gh-pages
 mv website/maven maven/$V
 rm -rf maven/latest
 cp -r maven/$V maven/latest
-git add .
+git add --all
 git commit -m "update maven site for v$V"
 git push
 git checkout master
