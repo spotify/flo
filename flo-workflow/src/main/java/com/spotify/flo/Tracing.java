49c948f5d7da711613130d60c03d55a09f532bd0
Daniel Norberg <dano@spotify.com>
2018-06-04 13:26:24 +0200

    seal Tracing class

--- flo-workflow/src/main/java/com/spotify/flo/Tracing.java
+++ flo-workflow/src/main/java/com/spotify/flo/Tracing.java
@@ -2,10 +2,13 @@ package com.spotify.flo;
 
 import io.grpc.Context;
 
-public class Tracing {
+public final class Tracing {
 
   public static final Context.Key<String> TASK_ID = Context.keyWithDefault("task-id", "");
   public static final Context.Key<String> TASK_NAME = Context.keyWithDefault("task-name", "");
   public static final Context.Key<String> TASK_ARGS = Context.keyWithDefault("task-args", "");
 
+  private Tracing() {
+    throw new UnsupportedOperationException();
+  }
 }
