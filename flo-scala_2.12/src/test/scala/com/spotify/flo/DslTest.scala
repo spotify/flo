eb796001deb5f504fafb985ddf4f6f6f736db421
Daniel Norberg <dano@spotify.com>
2018-06-14 15:04:08 +0900

    wip: fix forking for scala 2.11

--- flo-scala_2.12/src/test/scala/com/spotify/flo/DslTest.scala
+++ flo-scala_2.12/src/test/scala/com/spotify/flo/DslTest.scala
@@ -1,5 +1,8 @@
 package com.spotify.flo
 
+import java.lang.management.ManagementFactory
+
+import com.spotify.flo.context.FloRunner
 import org.scalatest._
 
 class DslTest extends FlatSpec with Matchers {
@@ -34,6 +37,28 @@ class DslTest extends FlatSpec with Matchers {
     val exception = the [RuntimeException] thrownBy $
 
     exception should have message "Builder accessor used outside a defTask scope"
+  }
+
+  it should "runInProcesses" in {
+    val foo = defTaskNamed("foo")
+      .process(() => {
+        val jvm = ManagementFactory.getRuntimeMXBean.getName
+        println("foo jvm: " + jvm)
+        jvm
+      })
+    val bar = defTaskNamed("bar")
+      .input(foo)
+      .context(new TaskContextStrict[String, Array[String]] {
+        override def provide(evalContext: EvalContext): String = "hello"
+      })
+      .process((fooName, _) => {
+        val jvm = ManagementFactory.getRuntimeMXBean.getName
+        println("bar jvm: " + jvm)
+        Array(fooName(), jvm)
+      })
+    val result: Array[String] = FloRunner.runTask(bar)
+      .future().get()
+    result.toSet should have size 2
   }
 
   def classMethod: Task[String] = defTask().process("hello")
