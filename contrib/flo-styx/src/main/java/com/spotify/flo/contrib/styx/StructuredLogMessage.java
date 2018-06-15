bb6652d50c99d3d04ab234a5dfa9cb593747672f
Honnix <honnix@users.noreply.github.com>
2018-06-15 11:24:23 +0200

    regroup log entry fields (#93)
    * regroup log entry fields

--- contrib/flo-styx/src/main/java/com/spotify/flo/contrib/styx/StructuredLogMessage.java
+++ contrib/flo-styx/src/main/java/com/spotify/flo/contrib/styx/StructuredLogMessage.java
@@ -22,32 +22,56 @@ package com.spotify.flo.contrib.styx;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.norberg.automatter.AutoMatter;
-import javax.annotation.Nullable;
 
 @AutoMatter
 interface StructuredLogMessage {
-  @JsonProperty @Nullable String time();
-  @JsonProperty @Nullable String severity();
-  @JsonProperty @Nullable String logger();
-  @JsonProperty @Nullable String thread();
-  @JsonProperty @Nullable String message();
-  @JsonProperty @Nullable String framework();
-  @JsonProperty @Nullable String styx_component_id();
-  @JsonProperty @Nullable String styx_workflow_id();
-  @JsonProperty @Nullable String styx_docker_args();
-  @JsonProperty @Nullable String styx_docker_image();
-  @JsonProperty @Nullable String styx_commit_sha();
-  @JsonProperty @Nullable String styx_parameter();
-  @JsonProperty @Nullable String styx_execution_id();
-  @JsonProperty @Nullable String styx_trigger_id();
-  @JsonProperty @Nullable String styx_trigger_type();
-  @JsonProperty @Nullable String task_id();
-  @JsonProperty @Nullable String task_name();
-  @JsonProperty @Nullable String task_args();
+  @AutoMatter
+  interface Styx {
+    @JsonProperty String component_id();
+    @JsonProperty String workflow_id();
+    @JsonProperty String docker_args();
+    @JsonProperty String docker_image();
+    @JsonProperty String commit_sha();
+    @JsonProperty String parameter();
+    @JsonProperty String execution_id();
+    @JsonProperty String trigger_id();
+    @JsonProperty String trigger_type();
 
-  StructuredLogMessageBuilder builder();
+    static StyxBuilder newBuilder() {
+      return new StyxBuilder();
+    }
+  }
+
+  @AutoMatter
+  interface Task {
+    @JsonProperty String id();
+    @JsonProperty String name();
+    @JsonProperty String args();
+
+    static TaskBuilder newBuilder() {
+      return new TaskBuilder();
+    }
+  }
+
+  @AutoMatter
+  interface Workflow {
+    @JsonProperty Styx styx();
+    @JsonProperty String framework();
+    @JsonProperty Task task();
+
+    static WorkflowBuilder newBuilder() {
+      return new WorkflowBuilder().framework("flo");
+    }
+  }
+
+  @JsonProperty String time();
+  @JsonProperty String severity();
+  @JsonProperty String logger();
+  @JsonProperty String thread();
+  @JsonProperty String message();
+  @JsonProperty Workflow workflow();
 
   static StructuredLogMessageBuilder newBuilder() {
-    return new StructuredLogMessageBuilder().framework("flo");
+    return new StructuredLogMessageBuilder();
   }
 }
--- contrib/flo-styx/src/main/java/com/spotify/flo/contrib/styx/StructuredLoggingEncoder.java
+++ contrib/flo-styx/src/main/java/com/spotify/flo/contrib/styx/StructuredLoggingEncoder.java
@@ -51,7 +51,7 @@ public class StructuredLoggingEncoder extends EncoderBase<ILoggingEvent> {
   private static final byte[] LINE_SEPARATOR_BYTES = LINE_SEPARATOR.getBytes();
 
   private final boolean isStyxExecution = System.getenv().containsKey("STYX_EXECUTION_ID");
-  private final StructuredLogMessage template = createTemplate();
+  private final StyxBuilder template = createTemplate();
 
   private final TaskId envTaskId;
 
@@ -71,32 +71,36 @@ public class StructuredLoggingEncoder extends EncoderBase<ILoggingEvent> {
   }
 
   private byte[] encodeStructured(ILoggingEvent event) {
-
-    // Pre-populated with styx metadata
-    final StructuredLogMessageBuilder builder = template.builder();
+    final StructuredLogMessageBuilder structuredLogMessageBuilder =
+        StructuredLogMessage.newBuilder();
 
     // Standard fields
-    builder.time(Instant.ofEpochMilli(event.getTimeStamp()).toString());
-    builder.severity(event.getLevel().toString());
-    builder.logger(event.getLoggerName());
-    builder.thread(event.getThreadName());
+    structuredLogMessageBuilder.time(Instant.ofEpochMilli(event.getTimeStamp()).toString());
+    structuredLogMessageBuilder.severity(event.getLevel().toString());
+    structuredLogMessageBuilder.logger(event.getLoggerName());
+    structuredLogMessageBuilder.thread(event.getThreadName());
     final StringBuilder message = new StringBuilder(event.getFormattedMessage());
     final IThrowableProxy t = event.getThrowableProxy();
     if (t != null) {
       message.append('\n');
       writeStack(message, t, "", 0, "\n");
     }
-    builder.message(message.toString());
+    structuredLogMessageBuilder.message(message.toString());
 
-    // Flo metadata
+    // Workflow metadata
+    final WorkflowBuilder workflowBuilder = StructuredLogMessage.Workflow.newBuilder()
+        .styx(template.build());
     final TaskId taskId = taskId();
-    builder.task_id(taskId != null ? taskId.toString() : "");
-    builder.task_name(taskId != null ? taskId.name() : "");
-    builder.task_args(taskId != null ? taskId.args() : "");
+    workflowBuilder.task(StructuredLogMessage.Task.newBuilder()
+        .id(taskId != null ? taskId.toString() : "")
+        .name(taskId != null ? taskId.name() : "")
+        .args(taskId != null ? taskId.args() : "")
+        .build());
+    structuredLogMessageBuilder.workflow(workflowBuilder.build());
 
     // Serialize to json
     try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-      WRITER.writeValue(baos, builder.build());
+      WRITER.writeValue(baos, structuredLogMessageBuilder.build());
       baos.write(LINE_SEPARATOR_BYTES);
       return baos.toByteArray();
     } catch (IOException e) {
@@ -174,18 +178,17 @@ public class StructuredLoggingEncoder extends EncoderBase<ILoggingEvent> {
     return null;
   }
 
-  private static StructuredLogMessage createTemplate() {
+  private static StyxBuilder createTemplate() {
     final Map<String, String> env = System.getenv();
-    return StructuredLogMessage.newBuilder()
-        .styx_component_id(env.getOrDefault("STYX_COMPONENT_ID", ""))
-        .styx_workflow_id(env.getOrDefault("STYX_WORKFLOW_ID", ""))
-        .styx_docker_args(env.getOrDefault("STYX_DOCKER_ARGS", ""))
-        .styx_docker_image(env.getOrDefault("STYX_DOCKER_IMAGE", ""))
-        .styx_commit_sha(env.getOrDefault("STYX_COMMIT_SHA", ""))
-        .styx_parameter(env.getOrDefault("STYX_PARAMETER", ""))
-        .styx_execution_id(env.getOrDefault("STYX_EXECUTION_ID", ""))
-        .styx_trigger_id(env.getOrDefault("STYX_TRIGGER_ID", ""))
-        .styx_trigger_type(env.getOrDefault("STYX_TRIGGER_TYPE", ""))
-        .build();
+    return StructuredLogMessage.Styx.newBuilder()
+        .component_id(env.getOrDefault("STYX_COMPONENT_ID", ""))
+        .workflow_id(env.getOrDefault("STYX_WORKFLOW_ID", ""))
+        .docker_args(env.getOrDefault("STYX_DOCKER_ARGS", ""))
+        .docker_image(env.getOrDefault("STYX_DOCKER_IMAGE", ""))
+        .commit_sha(env.getOrDefault("STYX_COMMIT_SHA", ""))
+        .parameter(env.getOrDefault("STYX_PARAMETER", ""))
+        .execution_id(env.getOrDefault("STYX_EXECUTION_ID", ""))
+        .trigger_id(env.getOrDefault("STYX_TRIGGER_ID", ""))
+        .trigger_type(env.getOrDefault("STYX_TRIGGER_TYPE", ""));
   }
 }
