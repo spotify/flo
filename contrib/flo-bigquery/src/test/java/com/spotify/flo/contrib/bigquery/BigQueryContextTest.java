bcd7578ea2d6d23fe504edff7dededfcca2474e9
Daniel Norberg <dano@spotify.com>
2018-07-31 17:24:56 +0900

    remove unused imports

--- contrib/flo-bigquery/src/test/java/com/spotify/flo/contrib/bigquery/BigQueryContextTest.java
+++ contrib/flo-bigquery/src/test/java/com/spotify/flo/contrib/bigquery/BigQueryContextTest.java
@@ -60,7 +60,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
